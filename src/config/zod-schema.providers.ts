07f6c195f0b0b0fccb153086e05b4e1fb4ba53f5
Barry Schneider (MSIH) <bschneider@msih.com>
2026-03-30 17:07:50 -0400

    Revert "merge(pr#29768): fix(message): add channels.defaults.defaultChannel to resolve ambiguity when multiple channels configured"
    This reverts commit 3b76d851c8d18752ba5c0de33508545556ef69fa, reversing
    changes made to 5bd88aba44b07e84226eb2a6f79e1960d0fbd4ba.

--- src/config/zod-schema.providers.ts
+++ src/config/zod-schema.providers.ts
@@ -108,7 +108,6 @@ export const ChannelsSchema: z.ZodType<ChannelsConfig | undefined> = z
       .object({
         groupPolicy: GroupPolicySchema.optional(),
         heartbeat: ChannelHeartbeatVisibilitySchema,
-        defaultChannel: z.string().optional(),
       })
       .strict()
       .optional(),
