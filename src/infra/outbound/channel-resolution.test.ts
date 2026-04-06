openclaw/openclaw

fix(plugins): use pinned channel registry for outbound message resolution
The outbound delivery path resolved channel plugins from the mutable active
plugin registry (getActivePluginRegistry), which can lose channel entries
when subagent activity triggers registry swaps. This caused 'Outbound not
configured for channel: signal' errors when subagents tried to send messages.

Changes:
- registry-loader.ts: use getActivePluginChannelRegistry (pinned) instead
  of getActivePluginRegistry (mutable) for outbound adapter loading
- channel-resolution.ts: check pinned channel registry first in both
  resolveDirectFromActiveRegistry and maybeBootstrapChannelPlugin, falling
  back to mutable registry for post-pin channels
- channel-resolution.test.ts: add 3 tests for the split-brain scenario

src/infra/outbound/channel-resolution.test.ts
@@ -5,6 +5,7 @@ const resolveAgentWorkspaceDirMock = vi.hoisted(() => vi.fn());
 const getChannelPluginMock = vi.hoisted(() => vi.fn());
 const applyPluginAutoEnableMock = vi.hoisted(() => vi.fn());
 const loadOpenClawPluginsMock = vi.hoisted(() => vi.fn());
+const getActivePluginChannelRegistryMock = vi.hoisted(() => vi.fn());
 const getActivePluginRegistryMock = vi.hoisted(() => vi.fn());
 const getActivePluginRegistryKeyMock = vi.hoisted(() => vi.fn());
 const normalizeMessageChannelMock = vi.hoisted(() => vi.fn());
@@ -28,6 +29,8 @@ vi.mock("../../plugins/loader.js", () => ({
 }));
 
 vi.mock("../../plugins/runtime.js", () => ({
+  getActivePluginChannelRegistry: (...args: unknown[]) =>
+    getActivePluginChannelRegistryMock(...args),
   getActivePluginRegistry: (...args: unknown[]) => getActivePluginRegistryMock(...args),
   getActivePluginRegistryKey: (...args: unknown[]) => getActivePluginRegistryKeyMock(...args),
 }));
@@ -53,6 +56,7 @@ describe("outbound channel resolution", () => {
     getChannelPluginMock.mockReset();
     applyPluginAutoEnableMock.mockReset();
     loadOpenClawPluginsMock.mockReset();
+    getActivePluginChannelRegistryMock.mockReset();
     getActivePluginRegistryMock.mockReset();
     getActivePluginRegistryKeyMock.mockReset();
     normalizeMessageChannelMock.mockReset();
@@ -64,6 +68,7 @@ describe("outbound channel resolution", () => {
     isDeliverableMessageChannelMock.mockImplementation((value?: string) =>
       ["telegram", "discord", "slack"].includes(String(value)),
     );
+    getActivePluginChannelRegistryMock.mockReturnValue(null);
     getActivePluginRegistryMock.mockReturnValue({ channels: [] });
     getActivePluginRegistryKeyMock.mockReturnValue("registry-key");
     applyPluginAutoEnableMock.mockReturnValue({ config: { autoEnabled: true } });
@@ -179,5 +184,73 @@ describe("outbound channel resolution", () => {
       cfg: { channels: {} } as never,
     });
     expect(loadOpenClawPluginsMock).toHaveBeenCalledTimes(2);
+  });
+
+  it("resolves from pinned channel registry when active registry lost the channel (subagent split-brain)", async () => {
+    const plugin = { id: "telegram" };
+    // getChannelPlugin returns the plugin (since the pinned registry feeds it)
+    getChannelPluginMock.mockReturnValue(plugin);
+    // Pinned channel registry has the channel
+    getActivePluginChannelRegistryMock.mockReturnValue({
+      channels: [{ plugin }],
+    });
+    // But mutable active registry has lost it (subagent swap evicted it)
+    getActivePluginRegistryMock.mockReturnValue({ channels: [] });
+
+    const channelResolution = await importChannelResolution("pinned-split-brain");
+
+    expect(
+      channelResolution.resolveOutboundChannelPlugin({
+        channel: "telegram",
+        cfg: {} as never,
+      }),
+    ).toBe(plugin);
+    // Should NOT attempt bootstrap since pinned registry has the channel
+    expect(loadOpenClawPluginsMock).not.toHaveBeenCalled();
+  });
+
+  it("falls back to direct pinned registry lookup when getChannelPlugin misses but pinned registry has entry", async () => {
+    const plugin = { id: "telegram" };
+    // getChannelPlugin misses (e.g. cache stale)
+    getChannelPluginMock.mockReturnValue(undefined);
+    // Pinned channel registry has the channel
+    getActivePluginChannelRegistryMock.mockReturnValue({
+      channels: [{ plugin }],
+    });
+    // Mutable active registry lost it
+    getActivePluginRegistryMock.mockReturnValue({ channels: [] });
+
+    const channelResolution = await importChannelResolution("pinned-direct-fallback");
+
+    expect(
+      channelResolution.resolveOutboundChannelPlugin({
+        channel: "telegram",
+        cfg: {} as never,
+      }),
+    ).toBe(plugin);
+    // No bootstrap needed — found in pinned registry
+    expect(loadOpenClawPluginsMock).not.toHaveBeenCalled();
+  });
+
+  it("skips bootstrap when pinned registry already has the requested channel", async () => {
+    const plugin = { id: "telegram" };
+    getChannelPluginMock
+      .mockReturnValueOnce(undefined) // first resolve() misses
+      .mockReturnValueOnce(undefined); // second resolve() also misses (would be after bootstrap)
+    // Pinned registry has the channel
+    getActivePluginChannelRegistryMock.mockReturnValue({
+      channels: [{ plugin }],
+    });
+    // Mutable registry does not
+    getActivePluginRegistryMock.mockReturnValue({ channels: [] });
+
+    const channelResolution = await importChannelResolution("pinned-skip-bootstrap");
+
+    channelResolution.resolveOutboundChannelPlugin({
+      channel: "telegram",
+      cfg: { channels: {} } as never,
+    });
+    // Should NOT bootstrap since pinned registry has the channel
+    expect(loadOpenClawPluginsMock).not.toHaveBeenCalled();
   });
 });

Arry8/openclaw-edge

src/infra/outbound/channel-resolution.test.ts
@@ -5,6 +5,7 @@ const resolveAgentWorkspaceDirMock = vi.hoisted(() => vi.fn());
 const getChannelPluginMock = vi.hoisted(() => vi.fn());
 const applyPluginAutoEnableMock = vi.hoisted(() => vi.fn());
 const resolveRuntimePluginRegistryMock = vi.hoisted(() => vi.fn());
+const getActivePluginChannelRegistryMock = vi.hoisted(() => vi.fn());
 const getActivePluginRegistryMock = vi.hoisted(() => vi.fn());
 const getActivePluginChannelRegistryVersionMock = vi.hoisted(() => vi.fn());
 const normalizeMessageChannelMock = vi.hoisted(() => vi.fn());
@@ -28,6 +29,8 @@ vi.mock("../../plugins/loader.js", () => ({
 }));
 
 vi.mock("../../plugins/runtime.js", () => ({
+  getActivePluginChannelRegistry: (...args: unknown[]) =>
+    getActivePluginChannelRegistryMock(...args),
   getActivePluginRegistry: (...args: unknown[]) => getActivePluginRegistryMock(...args),
   getActivePluginChannelRegistryVersion: (...args: unknown[]) =>
     getActivePluginChannelRegistryVersionMock(...args),
@@ -67,6 +70,7 @@ describe("outbound channel resolution", () => {
     getChannelPluginMock.mockReset();
     applyPluginAutoEnableMock.mockReset();
     resolveRuntimePluginRegistryMock.mockReset();
+    getActivePluginChannelRegistryMock.mockReset();
     getActivePluginRegistryMock.mockReset();
     getActivePluginChannelRegistryVersionMock.mockReset();
     normalizeMessageChannelMock.mockReset();
@@ -78,6 +82,7 @@ describe("outbound channel resolution", () => {
     isDeliverableMessageChannelMock.mockImplementation((value?: string) =>
       ["telegram", "discord", "slack"].includes(String(value)),
     );
+    getActivePluginChannelRegistryMock.mockReturnValue(null);
     getActivePluginRegistryMock.mockReturnValue({ channels: [] });
     getActivePluginChannelRegistryVersionMock.mockReturnValue(1);
     applyPluginAutoEnableMock.mockReturnValue({
@@ -206,5 +211,73 @@ describe("outbound channel resolution", () => {
       cfg: { channels: {} } as never,
     });
     expect(resolveRuntimePluginRegistryMock).toHaveBeenCalledTimes(2);
+  });
+
+  it("resolves from pinned channel registry when active registry lost the channel (subagent split-brain)", async () => {
+    const plugin = { id: "telegram" };
+    // getChannelPlugin returns the plugin (since the pinned registry feeds it)
+    getChannelPluginMock.mockReturnValue(plugin);
+    // Pinned channel registry has the channel
+    getActivePluginChannelRegistryMock.mockReturnValue({
+      channels: [{ plugin }],
+    });
+    // But mutable active registry has lost it (subagent swap evicted it)
+    getActivePluginRegistryMock.mockReturnValue({ channels: [] });
+
+    const channelResolution = await importChannelResolution("pinned-split-brain");
+
+    expect(
+      channelResolution.resolveOutboundChannelPlugin({
+        channel: "telegram",
+        cfg: {} as never,
+      }),
+    ).toBe(plugin);
+    // Should NOT attempt bootstrap since pinned registry has the channel
+    expect(resolveRuntimePluginRegistryMock).not.toHaveBeenCalled();
+  });
+
+  it("falls back to direct pinned registry lookup when getChannelPlugin misses but pinned registry has entry", async () => {
+    const plugin = { id: "telegram" };
+    // getChannelPlugin misses (e.g. cache stale)
+    getChannelPluginMock.mockReturnValue(undefined);
+    // Pinned channel registry has the channel
+    getActivePluginChannelRegistryMock.mockReturnValue({
+      channels: [{ plugin }],
+    });
+    // Mutable active registry lost it
+    getActivePluginRegistryMock.mockReturnValue({ channels: [] });
+
+    const channelResolution = await importChannelResolution("pinned-direct-fallback");
+
+    expect(
+      channelResolution.resolveOutboundChannelPlugin({
+        channel: "telegram",
+        cfg: {} as never,
+      }),
+    ).toBe(plugin);
+    // No bootstrap needed — found in pinned registry
+    expect(resolveRuntimePluginRegistryMock).not.toHaveBeenCalled();
+  });
+
+  it("skips bootstrap when pinned registry already has the requested channel", async () => {
+    const plugin = { id: "telegram" };
+    getChannelPluginMock
+      .mockReturnValueOnce(undefined) // first resolve() misses
+      .mockReturnValueOnce(undefined); // second resolve() also misses (would be after bootstrap)
+    // Pinned registry has the channel
+    getActivePluginChannelRegistryMock.mockReturnValue({
+      channels: [{ plugin }],
+    });
+    // Mutable registry does not
+    getActivePluginRegistryMock.mockReturnValue({ channels: [] });
+
+    const channelResolution = await importChannelResolution("pinned-skip-bootstrap");
+
+    channelResolution.resolveOutboundChannelPlugin({
+      channel: "telegram",
+      cfg: { channels: {} } as never,
+    });
+    // Should NOT bootstrap since pinned registry has the channel
+    expect(resolveRuntimePluginRegistryMock).not.toHaveBeenCalled();
   });
 });