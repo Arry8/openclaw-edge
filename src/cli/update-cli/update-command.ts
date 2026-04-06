smartchainark/openclaw

fix(update): use candidate resolution for doctor entrypoint in runPackageInstallUpdate

src/cli/update-cli/update-command.ts
@@ -296,8 +296,14 @@ async function runPackageInstallUpdate(params: {
 
   if (pkgRoot) {
     afterVersion = await readPackageVersion(pkgRoot);
-    const entryPath = path.join(pkgRoot, "dist", "entry.js");
-    if (await pathExists(entryPath)) {
+    let entryPath: string | undefined;
+    for (const candidate of resolveGatewayInstallEntrypointCandidates(pkgRoot)) {
+      if (await pathExists(candidate)) {
+        entryPath = candidate;
+        break;
+      }
+    }
+    if (entryPath) {
       const doctorStep = await runUpdateStep({
         name: `${CLI_NAME} doctor`,
         argv: [resolveNodeRunner(), entryPath, "doctor", "--non-interactive"],

Arry8/openclaw-edge

src/cli/update-cli/update-command.ts
@@ -411,8 +411,14 @@ async function runPackageInstallUpdate(params: {
         stdoutTail: null,
       });
     }
-    const entryPath = path.join(verifiedPackageRoot, "dist", "entry.js");
-    if (await pathExists(entryPath)) {
+    let entryPath: string | undefined;
+    for (const candidate of resolveGatewayInstallEntrypointCandidates(verifiedPackageRoot)) {
+      if (await pathExists(candidate)) {
+        entryPath = candidate;
+        break;
+      }
+    }
+    if (entryPath) {
       const doctorStep = await runUpdateStep({
         name: `${CLI_NAME} doctor`,
         argv: [resolveNodeRunner(), entryPath, "doctor", "--non-interactive"],